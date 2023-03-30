ee88d0ee9da0704a1f11b5a778e991feb2b62b44
E2-Madhumitha <you@example.com>
2023-03-30 17:41:49 +0530

    File value changes

--- lib/addContact.dart
+++ lib/addContact.dart
@@ -1,4 +1,8 @@
+
 import 'package:flutter/material.dart';
+
+import 'contact.dart';
+import 'listContact.dart';
 
 class ContactAdd2 extends StatelessWidget {
   const ContactAdd2({Key? key}) : super(key: key);
@@ -11,60 +15,98 @@ class ContactAdd2 extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Add Contact'),
         ),
-        body: const ContactAdd3(),
+        body: ContactAdd3(),
       ),
     );
   }
 }
 
+// ignore: must_be_immutable
 class ContactAdd3 extends StatelessWidget {
-  const ContactAdd3({Key? key}) : super(key: key);
+  TextEditingController nameController = TextEditingController();
+  TextEditingController contactController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
 
+  List<Contact> contacts = List.empty(growable: true);
+
+  ContactAdd3({super.key});
+
+  // ContactAdd3({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView(
-        children: const [
+        children: [
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+            padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 10),
             child: TextField(
-              decoration: InputDecoration(
+              controller: nameController,
+              decoration: const InputDecoration(
                   border: OutlineInputBorder(),
                   labelText: 'Name',
                   hintText: '@example(Ronaldo)'),
             ),
           ),
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+            padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 10),
             child: TextField(
-              decoration: InputDecoration(
+              controller: contactController,
+              decoration: const InputDecoration(
                   border: OutlineInputBorder(),
                   labelText: 'Mobile Number',
                   hintText: '@example(1234567890)'),
               keyboardType: TextInputType.number,
+              maxLength: 10,
             ),
           ),
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+            padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 10),
             child: TextField(
-              decoration: InputDecoration(
+              controller: emailController,
+              decoration: const InputDecoration(
                 border: OutlineInputBorder(),
-                labelText: 'Email-Id',
+                labelText: 'Emailid',
                 hintText: '@example(xyz123@gmail.com)',
               ),
             ),
           ),
           Padding(
-              padding: EdgeInsets.symmetric(horizontal: 120, vertical: 30),
-              child: FilledButton(
-                  onPressed: null,
-                  child: Text(
-                    'Submit',
-                    style: TextStyle(fontSize: 20),
-                  )))
+            padding: const EdgeInsets.symmetric(horizontal: 120, vertical: 30),
+            child: ElevatedButton(
+              onPressed: () {
+
+                String name = nameController.text.trim();
+                String contact = contactController.text.trim();
+                String email = emailController.text.trim();
+                if (name.isNotEmpty && contact.isNotEmpty) {
+                  contacts
+                      .add(Contact(name: name, contact: contact, email: email));
+                  // To clear the input field
+                  nameController.text = '';
+                  contactController.text = '';
+                  emailController.text = '';
+                  for(var i=0; i<contacts.length; i++){
+                    print(contacts[i].name);
+                    print(contacts[i].contact);
+                    print(contacts[i].email);
+                  }
+                }
+                Navigator.push(context,
+                  MaterialPageRoute(builder: (BuildContext context) => ContactAdd(
+                    name : name,
+                    contact : contact,
+                    email :email
+                  ))
+                );
+              },
+              child: const Text(
+                'Submit',
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+          ),
         ],
       ),
     );
-
   }
 }
--- lib/contact.dart
+++ lib/contact.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'addContact.dart';
+import 'listContact.dart';
 
 class AddContact extends StatelessWidget {
-  const AddContact({Key? key}) : super(key: key);
+  AddContact({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -14,69 +15,50 @@ class AddContact extends StatelessWidget {
           centerTitle: true,
           backgroundColor: Colors.grey,
         ),
-        body: const ContactAdd(),
+        body: ContactAdd(
+          name: '',
+          contact: '',
+          email: '',
+        ),
       ),
     );
   }
 }
 
 class ContactAdd extends StatelessWidget {
-  const ContactAdd({Key? key}) : super(key: key);
+  String name, contact, email;
+
+  ContactAdd(
+      {required this.name,
+      required this.contact,
+      required this.email});
+  List<Contact> contacts = List.empty(growable: true);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView(
-        children: const [
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 60, vertical: 14),
-            child: Align(
-              alignment: Alignment.topLeft,
-              child: TextButton(
-                onPressed: null,
-                child: Text(
-                  '9472318476',
-                  style: TextStyle(fontSize: 20, color: Colors.black),
-                ),
-              ),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 60, vertical: 14),
-            child: Align(
-              alignment: Alignment.topLeft,
-              child: TextButton(
-                  onPressed: null,
-                  child: Text(
-                    '6845982138',
-                    style: TextStyle(fontSize: 20, color: Colors.black),
-                  )),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 60, vertical: 14),
-            child: Align(
-              alignment: Alignment.topLeft,
-              child: TextButton(
-                  onPressed: null,
-                  child: Text(
-                    '6382169091',
-                    style: TextStyle(fontSize: 20, color: Colors.black),
-                  )),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 60, vertical: 14),
-            child: Align(
-              alignment: Alignment.topLeft,
-              child: TextButton(
-                  onPressed: null,
-                  child: Text(
-                    '6845982138',
-                    style: TextStyle(fontSize: 20, color: Colors.black),
-                  )),
-            ),
-          ),
+        children: [
+          contacts.add(name: '$name', contact: '$contact', email:'$email'),
+
+          Text('$name',
+            textAlign: TextAlign.center,
+            style: const TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
+          Text('$contact',
+            textAlign: TextAlign.center,
+          )
+          // contacts.isEmpty
+          //     ? const Text(
+          //         "No contact added",
+          //         textAlign: TextAlign.center,
+          //         style: TextStyle(fontSize: 15),
+          //       )
+          //     : Expanded(
+          //         child: ListView.builder(
+          //           itemCount: contacts.length,
+          //           itemBuilder: (context, index) => getRow(index),
+          //         ),
+          //       )
         ],
       ),
       floatingActionButton: FloatingActionButton(
@@ -95,3 +77,24 @@ class ContactAdd extends StatelessWidget {
     );
   }
 }
+
+// class Contact {
+//   String name;
+//   String contact;
+//   String email;
+//   Contact({required this.name, required this.contact, required this.email});
+// }
+// List<Contact> contacts = List.empty(growable: true);
+// Widget getRow(int index) {
+//   return ListTile(
+//     title: Column(
+//       children: [
+//         Text(
+//           contacts[index].name,
+//           style: const TextStyle(fontWeight: FontWeight.bold),
+//         ),
+//         Text(contacts[index].contact),
+//       ],
+//     ),
+//   );
+// }
